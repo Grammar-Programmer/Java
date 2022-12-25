4eeab657056feaef7a1e6afb7afc760dbd761bb4
Grammar-Programmer <101673051+Grammar-Programmer@users.noreply.github.com>
2022-12-25 20:00:46 +0000

    Update RGBtoCMYK.java

--- COS 126/Loops and Conditionals/Type conversion/RGBtoCMYK.java
+++ COS 126/Loops and Conditionals/Type conversion/RGBtoCMYK.java
@@ -1,1 +1,11 @@
+public class RGBtoCMYK {
 
+	public static void main(String[] args) {
+		System.out.println(
+				CMYK.formatRGBToCMYK(Integer.parseInt(args[0]), Integer.parseInt(args[1]), Integer.parseInt(args[2])));
+	}
+}
+
+/******************************************************************************
+ * Written By: Grammar-Programmer.
+ ******************************************************************************/
