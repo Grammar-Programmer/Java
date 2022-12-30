9e53c74f7c123eddc9b918c7a2c3266edbacec9c
Grammar-Programmer <101673051+Grammar-Programmer@users.noreply.github.com>
2022-12-30 16:50:37 +0000

    Create Marca.java

---
+++ POO/REVISOES_2FREQ/divisoesEletrodomesticos/Marca.java
@@ -0,0 +1,50 @@
+package divisoesEletrodomesticos;
+
+public enum Marca {
+	Admiral(), Alessi(), Amica(),
+
+	Bauknecht(), Bedazzler(), Beko(), Bertazzoni(), Bialetti(), Brandt(), Braun(), Breville(), Breville_Group(),
+	BSH_Hausgeräte(), Bticino(),
+
+	Candy(), Colston_Ariston(), Cuisinart(),
+
+	Damro(), Danby(), De_Dietrich_Remeha(), DeLonghi(), Donvier(), Duparquet_Huot_e_Moneuse(), Dustbot(),
+
+	Ecovacs_Robotics(), Edesa(), ELCO_Vagionis(), Electrolux(), Electrolux_Ankarsrum_Assistent(), Elica(), Eureka(),
+
+	Faema(), Frigidaire(), Fulgor_Milano(),
+
+	Gaggenau_Hausgeräte(), Gaggia(), Giacomini(), Gita(), Gongniu(), Gorenje(), GreeElectric(), Grundig(),
+
+	Haceb(), Haden(), Haier(), Hamilton_Beach_Brands(), Henry(),
+
+	IKEA(), Indesit(), InSinkErator(),
+
+	Kenmore(), Kent_RO_Systems(), Kleenmaid(),
+
+	La_Marzocco(), Lehel(), LG_Electronics(), LG_Tromm(), Lofra(),
+
+	Magic_Chef(), Malleable_Iron_Range_Company(), Maytag(), Medion(), Midea_Group(), Miele(), Mitsubishi_Electric(),
+	Mondial(), Morphy_Richards(), Moulinex(), Mr_Coffee(),
+
+	Neff_GmbH(), Nomiku(),
+
+	Olympic_Group(), Osterizer(), OXO(),
+
+	Panasonic(), Pars_Khazar(), Peerless_Premier_Appliance_Company(), Pifco(), Proctor_Silex(), Pyramis(),
+
+	Rancilio(), Rinnai(), Robert_Bosch_GmbH(), Roomba(), Rowenta(), Royalstar(), Russell_Hobbs(),
+
+	Saeco(), Samsung_Electronics(), Sanyo(), Scooba(), Servis(), Sharp_Corporation(), Simpson(), Skyworth(), Smeg(),
+	Snowa(), Speed_Queen(), Standex_International(), Sub_Zero(), Sunbeam(), Sunpentown(),
+
+	Tiger_Corporation(), Toastmaster(), TrueCookPlus(),
+
+	Ultimate_Chopper(),
+
+	Vestel(), Vestfrost(),
+
+	West_Bend_Company(), West_Bend_Housewares(), Whirlpool_Corporation(), Winia_Electronics(), Worcester_Bosch_Group(),
+
+	Zanussi(), Zojirushi(), Zyliss();
+}
