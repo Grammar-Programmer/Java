288699cf8dd5aee5ef2aa5f8145857b67dd2be10
Grammar-Programmer <101673051+Grammar-Programmer@users.noreply.github.com>
2022-12-30 16:50:53 +0000

    Create Divisao.java

---
+++ POO/REVISOES_2FREQ/divisoesEletrodomesticos/Divisao.java
@@ -0,0 +1,19 @@
+package divisoesEletrodomesticos;
+
+public enum Divisao {
+	Closet(),
+
+	Escritorio(),
+
+	Jardim_de_Inverno(),
+
+	Biblioteca(),
+
+	Sala_de_Cinema(),
+
+	Sala_de_Jogos(),
+
+	Ginasio(),
+
+	Bar(), Playroom();
+}
