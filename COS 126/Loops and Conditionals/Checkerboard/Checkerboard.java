bd3fb7c458d171a11fc107bb019827795f031b5f
Grammar-Programmer <101673051+Grammar-Programmer@users.noreply.github.com>
2023-01-04 00:51:31 +0000

    Create Checkerboard.java

---
+++ COS 126/Loops and Conditionals/Checkerboard/Checkerboard.java
@@ -0,0 +1,30 @@
+
+public class Checkerboard {
+
+	public static String getStringFormated(int numberPerLine, int numberPerColumn, String simbol, String separator) {
+		String str = "";
+
+		for (int i = 0; i < numberPerLine; i++) {
+			for (int j = 0; j < numberPerColumn; j++) {
+				str += simbol + separator;
+			}
+			str += "\n";
+			if (i % 2 == 0)
+				str += separator;
+		}
+
+		return str;
+	}
+
+	public static void main(String[] args) {
+		int numberPerLine = Integer.parseInt(args[0]);
+		int numberPerColumn = Integer.parseInt(args[0]);
+		String simbol = "*";
+		String separator = " ";
+		System.out.println(getStringFormated(numberPerLine, numberPerColumn, simbol, separator));
+	}
+}
+
+/******************************************************************************
+ * Written By: Grammar-Programmer.
+ ******************************************************************************/
